c50737aa0291d0f977e9db02255f129374e2dc29
alex <arcolistro@gmail.com>
2023-02-15 23:23:39 -0800

    Add apk resource, add driver session test file and set desired capabilities

--- src/test/java/CreateDriverSession.java
+++ src/test/java/CreateDriverSession.java
@@ -1,5 +1,15 @@
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class CreateDriverSession {
-    DesiredCapabilities caps = new DesiredCapabilities();
-}
+    public static void main(String[] args) {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "nexus_5");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+        caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+        caps.setCapability(MobileCapabilityType.APP,
+                "/Users/fieldemployee/IdeaProjects/FirstAppiumProject/src/main/resources/ApiDemos-debug.apk");
+        }
+    }
+
