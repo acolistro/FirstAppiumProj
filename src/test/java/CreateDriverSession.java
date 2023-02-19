65419bf66e562f92bac3c9887150983ba8585b73
alex <arcolistro@gmail.com>
2023-02-19 06:52:58 -0800

    Add code to open app without install, open app to specific activity and launch avd automatically w/ timeout

--- src/test/java/CreateDriverSession.java
+++ src/test/java/CreateDriverSession.java
@@ -14,10 +14,19 @@ public class CreateDriverSession {
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "nexus_5");
         caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiAutomator2");
         caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+        //launch avd automatically and close it automatically
+        caps.setCapability("avd", "Nexus_5_API_29");
+        //had to increase timeout from 180000 to 280000 to prevent 500 error
+        caps.setCapability("avdLaunchTimeout", 280000);
         //for windows users
         String appUrl = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
                 + File.separator + "resources" + File.separator + "ApiDemos-debug.apk";
+        //open previously installed app at a specific activity:
+        //caps.setCapability("appPackage", "io.appium.android.apis");
+        //caps.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+        //caps.setCapability("appActivity", "io.appium.android.apis.accessibility.CustomViewAccessibilityActivity");
         // "/Users/fieldemployee/IdeaProjects/FirstAppiumProject/src/main/resources/ApiDemos-debug.apk";
+       //Install and lunch app:
         caps.setCapability(MobileCapabilityType.APP, appUrl);
 
         //need to use localhost or 127.0.0.1, not 0.0.0.0 to fix "no session" error
