6db24f447cf8666631e4f9ef8518e21bc5309cb5
alex <arcolistro@gmail.com>
2023-02-21 10:33:37 -0800

    Add android ui automator class, after a lot of debugging I was able to fid a way to target a specific element in a list

--- src/test/java/AndroidFindElements.java
+++ src/test/java/AndroidFindElements.java
@@ -5,29 +5,29 @@ import org.openqa.selenium.WebElement;
 
 public class AndroidFindElements {
 
-    public static void main(String[] args) throws Exception {
-        AppiumDriver driver = CreateDriverSession.initializeDriver("Android");
-
-        WebElement myElement = (WebElement) driver.findElement(AppiumBy.accessibilityId("Accessibility"));
-        System.out.println(myElement.getText());
-
-        myElement = (WebElement) driver.findElement(AppiumBy.id("android:id/text1"));
-        System.out.println(myElement.getText());
-
-        myElement = (WebElement) driver.findElement(AppiumBy.className("android.widget.TextView"));
-        System.out.println(myElement.getText());
-
-        myElement = (WebElement) driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Accessibility\"]"));
-        System.out.println(myElement.getText());
-
-        myElement = (WebElement) driver.findElement(AppiumBy.xpath("//*[@text=\"Accessibility\"]"));
-        System.out.println(myElement.getText());
-
-        // error: Exception in thread "main" org.openqa.selenium.InvalidSelectorException: Locator Strategy 'name' is not supported for this session
-//        myElement = (WebElement) driver.findElement(AppiumBy.name("Accessibility"));
+//    public static void main(String[] args) throws Exception {
+//        AppiumDriver driver = CreateDriverSession.initializeDriver("Android");
+//
+//        WebElement myElement = (WebElement) driver.findElement(AppiumBy.accessibilityId("Accessibility"));
 //        System.out.println(myElement.getText());
+//
+//        myElement = (WebElement) driver.findElement(AppiumBy.id("android:id/text1"));
+//        System.out.println(myElement.getText());
+//
+//        myElement = (WebElement) driver.findElement(AppiumBy.className("android.widget.TextView"));
+//        System.out.println(myElement.getText());
+//
+//        myElement = (WebElement) driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Accessibility\"]"));
+//        System.out.println(myElement.getText());
+//
+//        myElement = (WebElement) driver.findElement(AppiumBy.xpath("//*[@text=\"Accessibility\"]"));
+//        System.out.println(myElement.getText());
+//
+//        // error: Exception in thread "main" org.openqa.selenium.InvalidSelectorException: Locator Strategy 'name' is not supported for this session
+////        myElement = (WebElement) driver.findElement(AppiumBy.name("Accessibility"));
+////        System.out.println(myElement.getText());
 
-    }
+    //}
 
 
 }
