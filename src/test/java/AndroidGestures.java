a5e3e012efd663557dd80c0aeec8b91cceea5a97
alex <arcolistro@gmail.com>
2023-02-21 13:39:06 -0800

    Add code for click gesture but it was not recognized as a command, add code for drag gesture

--- src/test/java/AndroidGestures.java
+++ src/test/java/AndroidGestures.java
@@ -1,6 +1,7 @@
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
 
@@ -8,7 +9,18 @@ public class AndroidGestures {
 
     public static void main(String[] args) throws Exception {
         AppiumDriver driver = CreateDriverSession.initializeDriver("Android");
-        longClickGesture(driver);
+        // longClickGesture(driver);
+        // clickGesture(driver);
+        dragGesture(driver);
+    }
+
+    // doesn't work - says click gesture isn't a recognized command
+    public static void clickGesture(AppiumDriver driver) {
+        WebElement element = driver.findElement(AppiumBy.accessibilityId("Views"));
+
+        driver.executeScript("mobile: clickGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) element).getId()
+        ));
     }
 
     public static void longClickGesture(AppiumDriver driver) {
@@ -25,4 +37,18 @@ public class AndroidGestures {
                 "duration", 1000
         ));
     }
+
+    public static void dragGesture(AppiumDriver driver) {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+        WebElement element = driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.view.View[1]"));
+
+        driver.executeScript("mobile: dragGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) element).getId(),
+                "endX", 649,
+                "endY", 662
+        ));
+
+    }
+
 }
